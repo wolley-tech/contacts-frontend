6e63a0484faab620c8ff01b75cfacaeb48d0688b
wolley-tech <wwsilva.tech@gmail.com>
2025-09-26 23:00:50 -0300

    fixing envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://wmessenger:8080/api',
+  apiBaseUrl: 'http://localhost/api',
   apiKey: '01998580-728c-7209-b549-de2c248f8d25'
 };
